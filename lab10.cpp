8ad235326b576661895ce77cf5dce55c5b21dc96
tymjames04 <tymjames04@gmail.com>
2025-11-04 20:14:18 -0600

    passing in works. Addition net yet implemented

--- lab10.cpp
+++ lab10.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 #include <string>
 using namespace std;
-
 class line{
 public:
+    //defines segment1 parts
     string seg1;
     char seg1sign = '+';
     string seg1left;
     string seg1right;
-
+    //defines segment2 parts
     string seg2;
     char seg2sign = '+';
     string seg2left;
@@ -18,7 +18,7 @@ public:
     void load(string &inputline){
         seg1 = inputline.substr(0, inputline.find(' '));
         seg2 = inputline.substr(inputline.find(' ') + 1);
-
+        //parsing and splitting up seg1
         int i1 = 0;
         if(seg1[0] == '+' || seg1[0] == '-'){
             i1++;
@@ -33,7 +33,7 @@ public:
         if(seg1[0] == '-'){
             seg1sign = '-';
         }
-
+        //parsing and splitting up seg2
         int i2 = 0;
         if(seg2[0] == '+' || seg2[0] == '-'){
             i2++;
@@ -52,19 +52,30 @@ public:
     }
 
     bool validate(){
+        if(seg1left.empty()) return false;
+        if(seg1right == "" && seg1.find('.') != string::npos) return false;
         for(int i = 0; i < seg1left.length(); i++){
             if(!isdigit(seg1left[i])) return false;
         }
         for(int i = 0; i < seg1right.length(); i++){
             if(!isdigit(seg1right[i])) return false;
         }
+        if(seg2left.empty()) return false;
+        if(seg2right == "" && seg2.find('.') != string::npos) return false;
         for(int i = 0; i < seg2left.length(); i++){
-            if(!isdigit(seg1left[i])) return false;
+            if(!isdigit(seg2left[i])) return false;
         }
         for(int i = 0; i < seg2right.length(); i++){
-            if(!isdigit(seg1right[i])) return false;
+            if(!isdigit(seg2right[i])) return false;
         }
         return true;
+    }
+
+    void add(){
+
+    }
+    void subtract(){
+
     }
 };
 
@@ -80,9 +91,9 @@ int main(){
         string mylineSeg1 = myline.seg1;
         string mylineSeg2 = myline.seg2;
         if(myline.validate()){
-            cout << "placeholder";
+            cout << "placeholder\n";
         }else{
-            cout << "Your input line is invalid. This line will be skipped";
+            cout << "Your input line is invalid. This line will be skipped\n";
         }
     }
 }
